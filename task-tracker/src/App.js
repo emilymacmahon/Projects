f20545c78863b5a467390d3d3f94774bafa7f39c
emilymacmahon <70771497+emilymacmahon@users.noreply.github.com>
2021-03-22 12:21:16 -0400

    starting up

---
+++ task-tracker/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+
+  const name ="emily";
+
+  return (
+    <div className="container">
+     <h1> Hello From React</h1>
+     <p> Hello {name} </p>
+    </div>
+  );
+}
+
+export default App;
